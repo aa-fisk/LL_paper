6e04144d3b8277d37743c7fafa7518dbc4880222
Angus Fisk <angus.fisk@bnc.ox.ac.uk>
2024-10-15 15:56:55 +1100

    Ascii bash script updated, needs to add template file

--- 02_analysis/10_edf_create/01_convert_to_edf.sh
+++ 02_analysis/10_edf_create/01_convert_to_edf.sh
@@ -2,40 +2,16 @@
 
 
 
-ascii="/Users/angusfisk/Documents/01_personal_files/01_work/09_github_repos/ascii2edf/ascii2edf.c"
-
-echo "hello world"
-
-# get template file 
-template_file=$(find "$PWD"/*.xml -maxdepth 1 -type f | head -n 1)
-echo $template_file
-
-# get list of files to iterate through
-file_dir="/Users/angusfisk/Documents/01_personal_files/01_work/11_LL_paper/02_analysis/01_data_files/02_txt"
-
-# use just first file debug
-first_file=$(find "$file_dir" -maxdepth 1 -type f | head -n 1)
-echo ${first_file}
-
-# create save file name
-save_name="${first_file%/*}/test_edf.edf"
-echo $save_name
+ascii="/Users/angusfisk/Documents/01_personal_files/01_work/09_github_repos/ascii2edf/ascii2edf"
 
 # apply ascii function 
-$asciif $first_file $template_file "Mouse" "date" "2018" "04" "09" "00" "00" "00" save_name
-
-# Good!
-#for f in "$file_dir"/*; do
-#    echo "$f"
-#
-#done 
-#!/bin/bash
+#$asciif $first_file $template_file "Mouse" "date" "2018" "04" "09" "00" "00" "00" save_name
 
 # Directory containing the input files
-input_directory="/path/to/your/directory"
+input_directory="/Users/angusfisk/Documents/01_personal_files/01_work/11_LL_paper/02_analysis/01_data_files/02_txt"
 
 # Directory to store the output EDF files
-output_directory="/path/to/output/directory"
+output_directory="/Users/angusfisk/Documents/01_personal_files/01_work/11_LL_paper/02_analysis/01_data_files/01_edf/01_script"
 
 # Create output directory if it doesn't exist
 mkdir -p "$output_directory"
@@ -46,10 +22,11 @@ for file in "$input_directory"/*; do
     if [[ -f "$file" ]]; then
         # Get the base name of the file (without the directory path)
         base_name=$(basename "$file")
+        echo $base_name
         
         # Run ascii2edf on the file
         # Change `ascii2edf` to the full path if necessary
-        ascii2edf "$file" -o "$output_directory/${base_name%.txt}.edf"  # Adjust file extension as needed
+        $ascii "$file" $template_file "Mouse" "date" "2018" "04" "09" "00" "00" "00" "$output_directory/${base_name%.txt}.edf"  # Adjust file extension as needed
 
         # Check if the command succeeded
         if [[ $? -eq 0 ]]; then
