352cda1369a783010a19b477c499ad2da3c63022
Angus Fisk <angus.fisk@bnc.ox.ac.uk>
2024-10-16 14:50:24 +1100

    Fixes edf and annotation files not lined up

--- 02_analysis/10_edf_create/03_edf_crop.py
+++ 02_analysis/10_edf_create/03_edf_crop.py
@@ -16,21 +16,32 @@ files = list(directory.glob('*.edf')) #raw_files
 state_files = list(state_dir.glob('*.hyp'))
 
 # select only files with annotations 
-state_file_basenames = {file.stem for file in state_files}
-filtered_edf_files = [file for file in files if file.stem in state_file_basenames]
+# Create a mapping from base filenames to their corresponding state files
+state_file_map = {file.stem: file for file in state_files}
+# Select only files with annotations
+filtered_edf_files = [file for file in files if file.stem in state_file_map]
 
-# 
-for file_test, curr_state in zip(filtered_edf_files, state_files): 
+
+
+# Iterate over files and corresponding annotations 
+for file_test in filtered_edf_files:
+    curr_state = state_file_map[file_test.stem]
+
     # read in file 
     signals, signal_headers, header = highlevel.read_edf(str(file_test), ch_names=ch_names)
     
     # read how long the file from header 
     curr_data = pd.read_csv(curr_state, sep='\t', header=None)
     duration = curr_data.iloc[0,1]
-   
+    print(file_test.stem) 
+    print("Duration: {}".format(duration))
+    print("Length of original data: {}".format(len(signals[0])/256))
+
     # crop signal to duration 
     day = int(256 * float(duration))
     signals_cropped = [arr[:day] for arr in signals]
+    
+    print("Length of cropped data: {}".format(len(signals_cropped[0])/256))
 
     # write file 
     save_name = save_dir / file_test.name
