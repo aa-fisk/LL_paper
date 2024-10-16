49884c108bf42a94a12eb467a7c62d407b4e2cb1
Angus Fisk <angus.fisk@bnc.ox.ac.uk>
2024-10-16 14:27:26 +1100

    Only crops files to correct length if have annotations

--- 02_analysis/10_edf_create/03_edf_crop.py
+++ 02_analysis/10_edf_create/03_edf_crop.py
@@ -1,26 +1,38 @@
 import os
+import pandas as pd
 from pathlib import Path
 from pyedflib import highlevel
 
-day = 256 * 86400
+# constants
 ch_names = ['chan. 0', 'chan. 1', 'chan. 2', 'chan. 3']
 
-
-# get list of files 
+# directories 
 directory = Path('/Users/angusfisk/Documents/01_personal_files/01_work/11_LL_paper/02_analysis/01_data_files/01_edf/01_script')
-files = list(directory.glob('*.edf'))
-file_test = files[0]
+state_dir = directory.parents[1] / '11_somnotate' / '05_visbrain' #annotations
+save_dir = directory.parent / '02_cropped'
 
+# get list of edf and annotations 
+files = list(directory.glob('*.edf')) #raw_files 
+state_files = list(state_dir.glob('*.hyp'))
 
-for file_test in files: 
+# select only files with annotations 
+state_file_basenames = {file.stem for file in state_files}
+filtered_edf_files = [file for file in files if file.stem in state_file_basenames]
+
+# 
+for file_test, curr_state in zip(filtered_edf_files, state_files): 
     # read in file 
     signals, signal_headers, header = highlevel.read_edf(str(file_test), ch_names=ch_names)
-
-    # crop signals down to 24 hours 
+    
+    # read how long the file from header 
+    curr_data = pd.read_csv(curr_state, sep='\t', header=None)
+    duration = curr_data.iloc[0,1]
+   
+    # crop signal to duration 
+    day = int(256 * float(duration))
     signals_cropped = [arr[:day] for arr in signals]
 
     # write file 
-    save_dir = directory.parent / '02_cropped'
     save_name = save_dir / file_test.name
     highlevel.write_edf(str(save_name), signals_cropped, signal_headers, header)
 
