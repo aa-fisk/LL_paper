e1408f175f1357fa7874e0a2ab391261b5dc08c9
Angus Fisk <angus.fisk@bnc.ox.ac.uk>
2024-10-18 11:15:55 +1100

    Sanity checks fft outputs

--- 02_analysis/02_analysis_files/06_explore/01_check_fft.py
+++ 02_analysis/02_analysis_files/06_explore/01_check_fft.py
@@ -5,23 +5,37 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 # Parameters
-fft_dir_path = Path('/Users/angusfisk/Documents/01_personal_files/01_work/11_LL_paper/02_analysis/01_data_files/07_clean_fft_files/01_script')
-channels = ['fro', 'occ', 'foc']  # Example channel names
+fft_dir_path = Path(
+    '/Users/angusfisk/Documents/01_personal_files/01_work/11_LL_paper/'
+    '02_analysis/01_data_files/07_clean_fft_files/01_script/fro'
+)
 
-files = list(fft_dir_path.glob('*.csv'))
+files = list(fft_dir_path.glob('LL4*'))
 curr_file = files[0]
 
 # read in data 
-data = pd.read_csv(curr_file, index_col=[-1, -2, -3])
+data = pd.read_csv(
+    curr_file, index_col=[0, -1], parse_dates=True
+).sort_index()
+#data = pd.read_csv(curr_file, index_col=[0,1])
+#for sleepsign fft files, header=17, skipfooter=(64825 - 2162))
 
-fro_data = data.xs("fro", level="Channel")
 
-plot_data = fro_data.reset_index(level=1, drop=True).drop("State", axis=1)
+fft_data = data.xs("fro", level=1)
 
+# resample so can actually plot 
+fft_data_resample = fft_data.drop("State", axis=1).resample("1min").mean()
 
+# try plotting 
 fig, ax = plt.subplots()
 
-ax.plot(plot_data.iloc[:, 0])
+ax.plot(fft_data_resample.iloc[:, 0])
 
 plt.show()
+
+
+
+
+# code for checking clean auto fft 
+
 
