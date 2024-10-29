f882e2bcb62af7c570bb32f38fe0731a5a3f5b79
Angus Fisk <angus.fisk@bnc.ox.ac.uk>
2024-10-30 10:34:10 +1100

    Removes MESA version

--- 02_analysis/02_analysis_files/period_estimate/period_find.py
+++ 02_analysis/02_analysis_files/period_estimate/period_find.py
@@ -32,39 +32,6 @@ ll_data = pir_data_clean.loc[ll_start:ll_end]
 # run period estimation (MESA)
 curr_data = ll_data.iloc[:, 3]
 
-def get_period_MESA(curr_data, hr_range = [19,26], figshow=False):
-    m = MESA()
-    m.solve(curr_data)
-
-    # define frequencies to use 
-    lower_hz = hr_range[0]/(60*60)  
-    upper_hz = hr_range[1]/(60*60) 
-    test_freq = np.linspace(upper_hz, lower_hz, 10000)  
-
-    density_values = m.spectrum(dt=10, frequencies=test_freq)
-
-    # Find the main frequency (peak in the power spectrum)
-    main_frequency_index = np.argmax(density_values)
-    main_frequency = test_freq[main_frequency_index]
-    main_period = main_frequency * (60*60)
-   
-    if figshow:
-        fig, ax = plt.subplots(2)
-
-        ax[0].loglog((test_freq * (60*60)), density_values)
-        ax[1].plot(curr_data)
-
-        fig.show()
-
-    return main_period
-
-period_list = []
-for col in ll_data:
-    curr_data = ll_data.loc[:, col] 
-    curr_period = get_period_MESA(curr_data, figshow=True)
-    period_list.append(curr_period)
-
-
 # periods = periodogram.get_period(ll_data)
 period_list = []
 hr_range = ["19h", "26h"]
@@ -82,8 +49,4 @@ for no, curr_data in enumerate(ll_data):
     period = (period_times.iloc[period_index].name[1]).total_seconds() / 3600
     period_list.append(period)
         
-
-
-
-
 # save results 
