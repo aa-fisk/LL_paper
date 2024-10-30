f8d5d6abb27e2bdb12100443ee82da50d07a458b
Angus Fisk <angus.fisk@bnc.ox.ac.uk>
2024-10-30 14:43:58 +1100

    Fixes Nan LombScargle with interpolation

--- 02_analysis/02_analysis_files/period_estimate/period_find.py
+++ 02_analysis/02_analysis_files/period_estimate/period_find.py
@@ -24,12 +24,13 @@ for curr_file in pir_files:
     unused_cols = list(pir_data.columns[i] for i in [0, 5, 6, 8])
     pir_data_clean = pir_data.drop(unused_cols, axis=1)
     pir_data_clean.index = pir_data.index.tz_localize(None)
-    pir_data_clean = pir_data_clean.resample('10s').mean()
-    
+    pir_data_resampled = pir_data_clean.resample('10s').mean()
+    pir_data_resampled = pir_data_resampled.interpolate()
+
     # select just the LL bit 
     ll_start = pd.to_datetime("2018-04-10 07:00:00")
     ll_end = pd.to_datetime("2018-04-25 07:00:00")
-    ll_data = pir_data_clean.loc[ll_start:ll_end]
+    ll_data = pir_data_resampled.loc[ll_start:ll_end]
     
     # set correct column names for concat
     if curr_file.stem == 'EEG_1':
@@ -44,49 +45,20 @@ all_data = pd.concat(ll_dfs, axis=1, join='inner')
 
 
 # calculate period for all animals 
-period_list = []
-for no, curr_data in enumerate(all_data):
-    print(no)
-    period_times = periodogram._period_df(
-        all_data, 
-        animal_no=no,
-        low_time=hr_range[0],
-        high_time=hr_range[1],
-        drop_level=False
-    )
-#    fig, ax = plt.subplots()
-#    ax.plot(period_times.index.get_level_values(1), period_times.values)
-#    fig.show()
-    period_index = np.argmax(period_times)
-    period = (period_times.iloc[period_index].name[1]).total_seconds() / 3600
-    period_list.append(period)
-        
+period_times = [periodogram._period_df(
+    all_data, 
+    animal_no=x,
+    low_time=hr_range[0],
+    high_time=hr_range[1],
+    drop_level=False
+) for x in range(len(all_data.columns))]
 
-# why not working? 
-# create x and y values of observations
-data = all_data
-animal_no = 0
-base_time = "10s"
-low_time = hr_range[0]
-high_time = hr_range[1]
-base_unit = "s"
-
-time = np.linspace(0, len(data), len(data))
-y = data.iloc[:, animal_no].values
-
-# convert all times into the same units (seconds)
-base_secs = pd.Timedelta(base_time).total_seconds()
-low_secs = pd.Timedelta(low_time).total_seconds()
-high_secs = pd.Timedelta(high_time).total_seconds()
-
-# frequency is number of 1/ cycles per base = base / cycles
-low_freq = base_secs / low_secs
-high_freq = base_secs / high_secs
-frequency = np.linspace(high_freq, low_freq, 1000)
-
-# find the LombScargle power at each frequency point
-power = LombScargle(time, y).power(frequency)
+# convert into hours 
+period_dict = {}
+for i, curr_period in enumerate(period_times):
+    period_index = np.argmax(curr_period)
+    period = (curr_period.iloc[period_index].name).total_seconds() / 3600
+    period_dict[all_data.columns[i]] = period
 
 
 
-# save results 
